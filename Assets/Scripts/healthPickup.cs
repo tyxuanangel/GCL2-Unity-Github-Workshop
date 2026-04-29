0c88354870cdc9cb45a90cd3bfe2c28fd85b11c5
Samara <samaraho3906@gmail.com>
2026-04-29 11:39:52 +0800

    [Samara] Added health recovery game object (healthPickup) and script (healthPickup.cs), froze player Z axis rotation

---
+++ Assets/Scripts/healthPickup.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class healthPickup : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player has restored 3 health points!");
+            Destroy(gameObject);
+        }
+    }
+}
